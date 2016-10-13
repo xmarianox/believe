2263734ecb7e73949a651cc5e11eaba44d0df00e
Mariano Molina <xsin.crueldadx@gmail.com>
2016-10-12 23:15:41 -0300

    :lipstick: add hamburguer button

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -5,4 +5,14 @@ $(document).ready(() => {
 	'use strict';
 
 	console.log('\'Allo \'Allo!');
+
+	/**
+	*  Menu
+	*/
+	$('.btn_lines').click(event => {
+		event.preventDefault();
+		$(event.currentTarget).toggleClass('close');
+		//$('menu').toggleClass('visible');
+	});
+
 });
